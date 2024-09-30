8a29191afd6478aa73af41c533070bb1bc4e691c
hai65 <vyastaroth123@gmail.com>
2024-09-30 09:39:07 +0700

    Prototype_ver_0.01

---
+++ Hair-Salon-Project/src/main/java/com/example/Hair_Salon_Project/HairSalonProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.Hair_Salon_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HairSalonProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HairSalonProjectApplication.class, args);
+	}
+
+}
