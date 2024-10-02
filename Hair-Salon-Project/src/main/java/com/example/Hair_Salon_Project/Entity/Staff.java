923f1c80ba1c6d634e2eeaa61834c339e7e01ece
hai65 <vyastaroth123@gmail.com>
2024-10-02 22:39:39 +0700

    4

--- Hair-Salon-Project/src/main/java/com/example/Hair_Salon_Project/Entity/Staff.java
+++ Hair-Salon-Project/src/main/java/com/example/Hair_Salon_Project/Entity/Staff.java
@@ -1,11 +1,17 @@
 package com.example.Hair_Salon_Project.Entity;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 import java.util.Date;
 
 public class Staff {
 
-    String staff_id;
-    String user_id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long staff_id;
+    long user_id;
     boolean staff_status;
     boolean is_manager;
     boolean is_active;
