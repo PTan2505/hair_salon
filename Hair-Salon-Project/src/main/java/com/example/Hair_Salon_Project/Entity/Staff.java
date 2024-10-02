f5d50cd3e1cef7352b9ef80f6217544a0cc3d47e
hai65 <vyastaroth123@gmail.com>
2024-10-02 22:25:39 +0700

    protocol ver 3

---
+++ Hair-Salon-Project/src/main/java/com/example/Hair_Salon_Project/Entity/Staff.java
@@ -0,0 +1,15 @@
+package com.example.Hair_Salon_Project.Entity;
+
+import java.util.Date;
+
+public class Staff {
+
+    String staff_id;
+    String user_id;
+    boolean staff_status;
+    boolean is_manager;
+    boolean is_active;
+    Date created_at;
+    Date updated_at;
+    String role;
+}
