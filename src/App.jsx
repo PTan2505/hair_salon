b8f294e48012f5421212aae65cc7280e0467a58d
PTan2505 <phuctann2505@gmail.com>
2024-10-12 20:42:03 +0700

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import AdminNavbar from "./navbar/Navbar";
-import Sidebar from "./sidebar/Sidebar";
+import Sidebar from "../sidebar/Sidebar";
 import Customer from "./customer/Customer";
 import Appointment from "./appointment/Appointment";
 import React from "react";
@@ -14,11 +14,11 @@ function App() {
         <Sidebar role="admin" />
         <div className="main-content">
           <Routes>
-            {/* <Route path={"/:endpoint"} element={<Customer />}></Route> */}
-            {/* <Route
+            <Route path={"/:endpoint"} element={<Customer />}></Route>
+            <Route
               path={"/appointment/:endpoint"}
               element={<Appointment />}
-            ></Route> */}
+            ></Route>
           </Routes>
         </div>
       </div>
