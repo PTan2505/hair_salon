f2048ae94525bf56fea431d70e025aa12c68925c
VuDuc20 <ducvase183704@fpt.edu.vn>
2024-10-17 11:39:56 +0700

    fix login

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,51 @@
 import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import HomePage from "./Page/Home";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
+import LoginPage from "./Page/Login";
+import RegisterPage from "./Page/Register";
+import ServiceWebsite from "./Page/Home";
+import Dashboard from "./components/dashboard";
+import { useSelector } from "react-redux";
+import { toast } from "react-toastify";
 
 function App() {
+  const ProtectRouteAuth = ({ children }) => {
+    const user = useSelector((store) => store);
+    console.log(user);
+    if (user && user?.role == "ADMIN") {
+      return children;
+    }
+    toast.error("You are not allowed to access this");
+    return <Navigate to={"/login"} />;
+  };
+
   const router = createBrowserRouter([
     {
       path: "",
-      element: <HomePage />,
+      element: <ServiceWebsite />,
+    },
+
+    {
+      path: "login",
+      element: <LoginPage />,
+    },
+
+    {
+      path: "register",
+      element: <RegisterPage />,
+    },
+    {
+      path: "dashboard",
+      element: (
+        <ProtectRouteAuth>
+          <Dashboard />
+        </ProtectRouteAuth>
+      ),
     },
   ]);
-
   return <RouterProvider router={router} />;
 }
 
