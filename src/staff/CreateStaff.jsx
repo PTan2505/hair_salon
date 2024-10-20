8f442b99c38455b7f1825c8348b236a5f63483d4
PTan2505 <khoapnse184368@fpt.edu.vn>
2024-10-21 00:36:19 +0700

    update

--- src/staff/CreateStaff.jsx
+++ src/staff/CreateStaff.jsx
@@ -1,77 +1,151 @@
+import { useFormik } from "formik";
 import React, { useContext, useState } from "react";
-import { Button, Form, Modal } from "react-bootstrap";
 import { StaffContext } from "../context/StaffContext";
+import * as yup from "yup";
+import { Button, Col, Form, Modal, Row } from "react-bootstrap";
+import Confirm from "../shared/Modal/Confirm";
 import { toastSuccess } from "../shared/toastify";
 
-export default function CreateStaff({ showModal, setShowModal }) {
+const AddStaff = ({ showModal, setShowModal, currentUserRole }) => {
+  const [formValues, setFormValues] = useState(null);
   const { handleAddStaff } = useContext(StaffContext);
-  const [newStaff, setNewStaff] = useState({
-    name: "",
-    phone: "", // Thêm trường SĐT
-    email: "",
-    gender: "", // Thêm trường giới tính
-    role: "", // Thêm trường role
-  });
+  const [showConfirm, setShowConfirm] = useState(false);
 
-  const handleChange = (e) => {
-    setNewStaff({
-      ...newStaff,
-      [e.target.name]: e.target.value,
-    });
+  const handleConfirm = (resetForm) => {
+    if (formValues) {
+      handleAddStaff(formValues);
+    }
+    setShowConfirm(false);
+    setShowModal(false);
+    resetForm();
+    toastSuccess("Add Staff Successfully");
   };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      await handleAddStaff(newStaff);
-      toastSuccess("Staff Added Successfully!");
-      setShowModal(false);
-      setNewStaff({
-        name: "",
-        phone: "", // Đặt lại trường SĐT
-        email: "",
-        gender: "", // Đặt lại trường giới tính
-        role: "", // Đặt lại trường role
-      });
-    } catch (error) {
-      console.error("Failed to add staff:", error);
+  const handleCancel = () => {
+    setShowConfirm(false);
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      phone: "",
+      role: "",
+    },
+    onSubmit: (values) => {
+      setFormValues(values);
+      setShowConfirm(true);
+    },
+    validationSchema: yup.object().shape({
+      phone: yup
+        .string()
+        .required("Phone number is required.")
+        .matches(/^[0-9]+$/, "Phone number must only contain digits")
+        .min(10, "Phone number must be at least 10 digits"),
+      role: yup
+        .string()
+        .required("Please select role.")
+        .test("role-not-none", "Choose a valid role", function (value) {
+          return value !== "none";
+        }),
+    }),
+  });
+
+  // Function to determine available roles based on the current user's role
+  const getAvailableRoles = () => {
+    if (currentUserRole === "SUPER_USER") {
+      return [
+        { value: "stylist", label: "Stylist" },
+        { value: "cashier", label: "Cashier" },
+        { value: "manager", label: "Manager" },
+      ];
+    } else if (currentUserRole === "MANAGER") {
+      return [
+        { value: "stylist", label: "Stylist" },
+        { value: "cashier", label: "Cashier" },
+      ];
+    } else {
+      return []; // No roles available for other types of users (if needed)
     }
   };
 
   return (
-    <Modal show={showModal} onHide={() => setShowModal(false)}>
-      <Modal.Header closeButton>
-        <Modal.Title>Add New Staff</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <Form onSubmit={handleSubmit}>
-          <Form.Group className="mb-3">
-            <Form.Label>Name</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Enter staff name"
-              name="name"
-              value={newStaff.name}
-              onChange={handleChange}
-              required
-            />
-          </Form.Group>
-          <Form.Group className="mb-3">
-            <Form.Label>Role</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Enter staff role"
-              name="role"
-              value={newStaff.role}
-              onChange={handleChange}
-              required
-            />
-          </Form.Group>
-          <Button variant="primary" type="submit">
-            Add Staff
-          </Button>
+    <>
+      <Confirm
+        action={"add"}
+        showConfirm={showConfirm}
+        onConfirm={() => handleConfirm(formik.resetForm)}
+        onCancel={handleCancel}
+      />
+      <Modal
+        show={showModal}
+        onHide={() => setShowModal(false)}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Add New Staff
+          </Modal.Title>
+        </Modal.Header>
+        <Form noValidate onSubmit={formik.handleSubmit}>
+          <Modal.Body>
+            <Row className="mb-3">
+              <Form.Group as={Col} controlId="validationFormikPhone">
+                <Form.Label>Phone</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="phone"
+                  placeholder="Enter phone number"
+                  value={formik.values.phone}
+                  onChange={formik.handleChange}
+                  isInvalid={formik.touched.phone && !!formik.errors.phone}
+                  isValid={formik.touched.phone && !formik.errors.phone}
+                />
+                {formik.errors.phone ? (
+                  <Form.Control.Feedback type="invalid">
+                    {formik.errors.phone}
+                  </Form.Control.Feedback>
+                ) : (
+                  <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                )}
+              </Form.Group>
+              <Form.Group as={Col} controlId="validationFormikRole">
+                <Form.Label>Role</Form.Label>
+                <Form.Select
+                  name="role"
+                  value={formik.values.role}
+                  aria-label="Select role"
+                  onChange={formik.handleChange}
+                  isInvalid={formik.touched.role && !!formik.errors.role}
+                  isValid={formik.touched.role && !formik.errors.role}
+                >
+                  <option value={"none"}>-------------</option>
+                  {getAvailableRoles().map((role) => (
+                    <option key={role.value} value={role.value}>
+                      {role.label}
+                    </option>
+                  ))}
+                </Form.Select>
+                {formik.errors.role ? (
+                  <Form.Control.Feedback type="invalid">
+                    {formik.errors.role}
+                  </Form.Control.Feedback>
+                ) : (
+                  <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                )}
+              </Form.Group>
+            </Row>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={() => setShowModal(false)}>Close</Button>
+            <Button type="submit">Add Staff</Button>
+          </Modal.Footer>
         </Form>
-      </Modal.Body>
-    </Modal>
+      </Modal>
+    </>
   );
-}
+};
+
+export default AddStaff;
